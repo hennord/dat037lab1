e1a9a799a8ce58331b971b189a3f8eb226130798
hennord <44836196+hennord@users.noreply.github.com>
2018-11-09 17:01:21 +0100

    Add files via upload

--- BinSearchIntSet.java
+++ BinSearchIntSet.java
@@ -1,32 +1,53 @@
 
-public class LinSearchIntSet implements IntSet {
+public class BinSearchIntSet implements IntSet {
 	public int size;
 	public int[] set;
 	
 	public void add(int element) {
+		int indexAdd;
 		if(!contains(element)) {
-			if(set.length-1 >= size) {
-					set[size] = element;
-					size++;
-			}else{
+			if(set.length == size) {
 				int[] tempset = new int[(2*set.length)];
 				for(int i =0; i < set.length;i++) {
 					tempset[i] = set[i];
 				}
-					tempset[size] = element;
-					size++;
-					set = tempset;
+				set = tempset;
+			}
+			if(size==0) {
+				set[size] = element;
+				size++;
+			}else {
+				indexAdd = size;
+				while(indexAdd >=1) {
+					if(element < set[indexAdd-1]){
+						indexAdd--;
+					}else {
+						break;
+					}
+				}
+				for(int j=size; j>=indexAdd+1;j--) {
+					set[j] = set[j-1];
+					}
+				set[indexAdd]=element;
+				size++;	
 			}
 		}
 	}
-	
+
 	public boolean contains(int element) {
 		if(size == 0) {
 			return false;
 		}else {
-			for(int x = 0; x < size; x++) {
-				if(element == set[x]){
+			int lower = 0;
+			int upper = size-1;
+			while(lower <= upper) {
+				int mid = (upper-lower)/2+lower;
+				if(element == set[mid]){
 					return true;
+				}else if(element >= set[mid]) {
+					lower = mid+1;
+				}else {
+					upper = mid-1;
 				}
 			}
 			return false;
@@ -56,7 +77,7 @@ public class LinSearchIntSet implements IntSet {
 		
 	}
 	
-	public LinSearchIntSet() {
+	public BinSearchIntSet() {
 		size = 0;
 		set = new int[1];
 	}
